89751910441f7893c04a254c4ff182b05cae0bfd
Hazeem <hazeemluqman03@gmail.com>
2024-12-30 22:03:37 +0800

    Remove unused SCSS files and update home view to extend master layout

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,39 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/academicians">
+                <i class="bi bi-person"></i>
+                <span>Academicians</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/grants">
+                <i class="bi bi-envelope"></i>
+                <span>Grants</span>
+            </a>
+        </li><!-- End Contact Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/milestones">
+                <i class="bi bi-card-list"></i>
+                <span>Milestones</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+
+    </ul>
+
+</aside><!-- End Sidebar-->
